ba728950d17b5a1b150f340627335b3b05978ae0
Hamza-Seo-Expert <seoexperthamza9@gmail.com>
2026-04-22 12:26:04 +0500

    Added dynamic export to robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,9 +1,3 @@
-import type { MetadataRoute } from "next";
+export const dynamic = "force-static";
 
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: [{ userAgent: "*", allow: "/" }],
-    sitemap: "https://www.invisible-character.net/sitemap.xml",
-    host: "https://www.invisible-character.net",
-  };
-}
+// Existing content...
